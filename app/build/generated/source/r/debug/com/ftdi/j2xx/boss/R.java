8fc7143f522f1d235767011dc8a20789ad8127bf
Jaxxs <mark@bltech.co.nz>
2015-03-19 13:22:05 +1300

    Fix UI Bug

--- app/build/generated/source/r/debug/com/ftdi/j2xx/boss/R.java
+++ app/build/generated/source/r/debug/com/ftdi/j2xx/boss/R.java
@@ -32,40 +32,41 @@ public final class R {
         public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int ClearButton=0x7f070006;
-        public static final int ConfigButton=0x7f07001e;
-        public static final int FireRateButton=0x7f07000d;
-        public static final int GasDelayButton=0x7f070007;
-        public static final int IgnGapButton=0x7f07000b;
-        public static final int MvDetectButton=0x7f07000f;
-        public static final int ReadField=0x7f070003;
-        public static final int ReadValues=0x7f070004;
-        public static final int SparkDelayButton=0x7f070009;
-        public static final int StatButton=0x7f070005;
-        public static final int baudRate=0x7f070012;
-        public static final int baudRateValue=0x7f070013;
+        public static final int ClearButton=0x7f070007;
+        public static final int ConfigButton=0x7f07001f;
+        public static final int FireRateButton=0x7f07000e;
+        public static final int GasDelayButton=0x7f070008;
+        public static final int IgnGapButton=0x7f07000c;
+        public static final int MvDetectButton=0x7f070010;
+        public static final int ReadField=0x7f070004;
+        public static final int ReadValues=0x7f070005;
+        public static final int SparkDelayButton=0x7f07000a;
+        public static final int StatButton=0x7f070006;
+        public static final int UartInfo=0x7f070003;
+        public static final int baudRate=0x7f070013;
+        public static final int baudRateValue=0x7f070014;
         public static final int createFileName=0x7f070002;
-        public static final int dataBit=0x7f070016;
-        public static final int dataBitValue=0x7f070017;
+        public static final int dataBit=0x7f070017;
+        public static final int dataBitValue=0x7f070018;
         public static final int filenamedialog=0x7f070000;
-        public static final int flowControl=0x7f07001a;
-        public static final int flowControlValue=0x7f07001b;
-        public static final int keyCtrlC=0x7f070020;
-        public static final int keyESC=0x7f070021;
-        public static final int menuSettings=0x7f070011;
-        public static final int menuSpecialKey=0x7f07001f;
-        public static final int parity=0x7f070018;
-        public static final int parityValue=0x7f070019;
-        public static final int portSelect=0x7f07001c;
-        public static final int portSelectValue=0x7f07001d;
-        public static final int stopBit=0x7f070014;
-        public static final int stopBitValue=0x7f070015;
+        public static final int flowControl=0x7f07001b;
+        public static final int flowControlValue=0x7f07001c;
+        public static final int keyCtrlC=0x7f070021;
+        public static final int keyESC=0x7f070022;
+        public static final int menuSettings=0x7f070012;
+        public static final int menuSpecialKey=0x7f070020;
+        public static final int parity=0x7f070019;
+        public static final int parityValue=0x7f07001a;
+        public static final int portSelect=0x7f07001d;
+        public static final int portSelectValue=0x7f07001e;
+        public static final int stopBit=0x7f070015;
+        public static final int stopBitValue=0x7f070016;
         public static final int tvname=0x7f070001;
-        public static final int txtFireRate=0x7f07000e;
-        public static final int txtGasDelay=0x7f070008;
-        public static final int txtIngGap=0x7f07000c;
-        public static final int txtMvDetect=0x7f070010;
-        public static final int txtSparkDelay=0x7f07000a;
+        public static final int txtFireRate=0x7f07000f;
+        public static final int txtGasDelay=0x7f070009;
+        public static final int txtIngGap=0x7f07000d;
+        public static final int txtMvDetect=0x7f070011;
+        public static final int txtSparkDelay=0x7f07000b;
     }
     public static final class layout {
         public static final int file_name_dialog=0x7f030000;
@@ -76,6 +77,7 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int baudRate=0x7f060001;
         public static final int gasDelays=0x7f060002;
+        public static final int spark_duration=0x7f060003;
     }
     public static final class xml {
         public static final int device_filter=0x7f040000;
